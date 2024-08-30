74f71cabab73d6745db2479699958c61123172df
Lev Vereshchagin <mail@vrslev.com>
2024-08-30 22:48:45 +0300

    Update

--- lessons_reporter_bot/main.py
+++ lessons_reporter_bot/main.py
@@ -1,11 +1,14 @@
+import inspect
 from collections import defaultdict
 from contextlib import suppress
+from functools import partial
+from typing import Any, Literal
 
 import telebot
 from pydantic import ValidationError
 from sqlmodel import SQLModel, create_engine
 from telebot.apihelper import ApiTelegramException
-from telebot.types import CallbackQuery, Message
+from telebot.types import CallbackQuery, InlineKeyboardMarkup, Message
 from telebot.util import quick_markup
 
 from lessons_reporter_bot.authorization_service import AuthorizationService
@@ -38,9 +41,11 @@ from lessons_reporter_bot.callback_data import (
     UpdateStudentNameCallbackData,
     any_callback_data_validator,
 )
+from lessons_reporter_bot.callback_storage import CallbackStorage
 from lessons_reporter_bot.models import (
     BotServiceMessage,
     BotServiceRegisterNextMessageHandler,
+    FormattedPaginationItem,
     HomeworkStatus,
 )
 from lessons_reporter_bot.report_builder import ReportBuilder
@@ -48,7 +53,7 @@ from lessons_reporter_bot.report_storage import ReportStorage
 from lessons_reporter_bot.settings import Settings
 from lessons_reporter_bot.student_storage import StudentStorage
 from lessons_reporter_bot.topic_storage import TopicStorage
-from lessons_reporter_bot.utils import FIRST_PAGE
+from lessons_reporter_bot.utils import FIRST_PAGE, paginate
 
 settings = Settings()
 
@@ -66,247 +71,141 @@ bot_service = BotService(
     report_storage=report_storage,
     authorization_service=authorization_service,
 )
+
 telegram_bot = telebot.TeleBot(token=settings.bot_token)
+callbacks = CallbackStorage(callbacks_version='1')
 
-LAST_MESSAGE_IDS: dict[int, list[int]] = defaultdict(list)
+
+def _convert_button_dict(
+    buttons: dict[str, dict[str, Any]],
+) -> dict[str, dict[str, Any]]:
+    if callback_data := buttons.get('callback_data'):
+        assert isinstance(callback_data, partial)
+        buttons['callback_data'] = callbacks.to_callback_data(callback_data)
+    return buttons
 
 
-def process_bot_service_handler_results(
-    *results: BotServiceMessage | BotServiceRegisterNextMessageHandler, chat_id: int
-) -> Message:
-    for result in results:
-        match result:
-            case BotServiceMessage() as message:
-                telegram_bot.clear_step_handler_by_chat_id(chat_id)
+def build_quick_markup(
+    buttons: dict[str, partial], row_width: int = 2
+) -> InlineKeyboardMarkup:
+    return quick_markup(
+        values={
+            name: _convert_button_dict(value) for (name, value) in buttons.values()
+        },
+        row_width=row_width,
+    )
 
-                if authorization_service.has_teacher_access(user_id=chat_id):
-                    for message_id in LAST_MESSAGE_IDS[chat_id]:
-                        # Ignore if the message is already deleted or not found
-                        with suppress(ApiTelegramException):
-                            telegram_bot.delete_message(chat_id, message_id)
+    # return BotServiceMessage(
+    #     text='Главное меню:',
+    #     buttons=[
+    #         BotServiceMessageButton(
+    #             title='Студенты',
+    #             callback_data=ShowItemsListCallbackData(i_t='S', i_f=None, page=1),
+    #         ),
+    #         BotServiceMessageButton(
+    #             title='Темы уроков',
+    #             callback_data=ShowItemsListCallbackData(i_t='T', i_f=None, page=1),
+    #         ),
+    #         BotServiceMessageButton(
+    #             title='Отчёты',
+    #             callback_data=ShowItemsListCallbackData(i_t='R', i_f=None, page=1),
+    #         ),
+    #         BotServiceMessageButton(
+    #             title='Составить отчёт',
+    #             callback_data=ReportBuilder1CallbackData(),
+    #         ),
+    #     ],
+    # )
 
-                for button in message.buttons:
-                    print(
-                        len(button.callback_data.model_dump_json().encode()),
-                        'bytes',
-                        button.callback_data,
-                    )
-                buttons = {
-                    button.title: {
-                        'callback_data': button.callback_data.model_dump_json()
-                    }
-                    for button in message.buttons
-                }
-                reply_markup = (
-                    quick_markup(buttons, row_width=message.row_width)
-                    if message.buttons
-                    else None
-                )
-                sent_message = telegram_bot.send_message(
-                    chat_id,
-                    message.text,
-                    reply_markup=reply_markup,
-                    parse_mode='MARKDOWN',
-                )
-                if authorization_service.has_teacher_access(user_id=chat_id):
-                    LAST_MESSAGE_IDS[chat_id].append(sent_message.message_id)
-            case BotServiceRegisterNextMessageHandler():
 
-                def callback(message: Message) -> None:
-                    process_bot_service_handler_results(
-                        *result.callback(message.text), chat_id=chat_id
-                    )
-
-                telegram_bot.register_next_step_handler_by_chat_id(
-                    chat_id=chat_id, callback=callback
-                )
+def build_menu_buttons() -> InlineKeyboardMarkup:
+    return build_quick_markup(
+        {
+            'Студенты': {'callback_data': partial(show_student_list, 1)},
+            'Темы уроков': {'callback_data': ''},
+            'Отчёты': {'callback_data': ''},
+            'Составить отчёт': {'callback_data': ''},
+        }
+    )
 
 
 @telegram_bot.message_handler(['start', 'help'])
 def welcome(message: Message) -> None:
-    user_id = message.from_user.id
-    process_bot_service_handler_results(*bot_service.welcome(user_id), chat_id=user_id)
+    if authorization_service.has_teacher_access(message.from_user.id):
+        telegram_bot.send_message(
+            message.chat.id, text=f'Главное меню:', reply_markup=build_menu_buttons()
+        )
+    else:
+        telegram_bot.send_message(
+            message.chat.id,
+            text=f'Здравствуйте! Перешлите это сообщение Елене Петровне:\n`{message.from_user.id}`',
+        )
+        telegram_bot.send_message(
+            message.chat.id,
+            text='После этого бот будет присылать вам отчёты о проведённых занятиях. Если возникли вопросы, обратитесь к Елене Петровне. Спасибо!',
+        )
 
 
 @telegram_bot.callback_query_handler(lambda call: call)
 def catchall_callback_handler(call: CallbackQuery) -> None:
-    user_id = call.from_user.id
-    match data := any_callback_data_validator.validate_json(call.data):
-        case GoBackToAdminPanelCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.welcome(user_id), chat_id=user_id
-            )
+    callback_partial = callbacks.from_callback_data(call.data)
 
-        case CreateTopicCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.create_topic(data), chat_id=user_id
-            )
+    args = inspect.getfullargspec(callback_partial.func).args
+    if 'call' in args:
+        callback_partial.keywords['call'] = call
+    if 'chat_id' in args:
+        callback_partial.keywords['chat_id'] = call.from_user.id
+    if 'message_id' in args:
+        callback_partial.keywords['message_id'] = call.message.id
 
-        case CreateStudentCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.create_student(data), chat_id=user_id
-            )
+    callback_partial()
 
-        case AddParentIdToStudentCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.add_parent_id_to_student(data, student_id=data.student_id),
-                chat_id=user_id,
-            )
 
-        case UpdateStudentNameCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.update_student_name(data), chat_id=user_id
-            )
+@callbacks.register
+def show_one_student(item_id: int, current_page: int) -> None: ...
 
-        case ReportBuilder1CallbackData():
-            report_builder.clear_temp_report()
-            process_bot_service_handler_results(
-                bot_service.build_report_1_lesson_date_setting(), chat_id=user_id
-            )
 
-        case ReportBuilder1SetValueFromButtonCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.build_report_1_lesson_date_from_button(
-                    lesson_day=data.lesson_day
-                ),
-                chat_id=user_id,
-            )
+@callbacks.register
+def show_menu(call: CallbackQuery) -> BotServiceMessage:
+    telegram_bot.edit_message_text(
+        chat_id=call.from_user.id,
+        message_id=call.message.id,
+        text=f'Главное меню:',
+        reply_markup=build_menu_buttons(),
+    )
 
-        case ReportBuilder1EnterManuallyCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.build_report_1_manual(), chat_id=user_id
-            )
 
-        case ReportBuilderShowItemListCallbackData():
-            if data.i_t == 'T':
-                process_bot_service_handler_results(
-                    bot_service.build_report_2_topic_setting(
-                        data=ReportBuilderShowItemListCallbackData(
-                            i_t='T', page=data.page
-                        ),
-                    ),
-                    chat_id=user_id,
-                )
-            elif data.i_t == 'S':
-                process_bot_service_handler_results(
-                    bot_service.build_report_3_student_setting(
-                        data=ReportBuilderShowItemListCallbackData(
-                            i_t='S', page=data.page
-                        ),
-                    ),
-                    chat_id=user_id,
-                )
+@callbacks.register
+def show_student_list(current_page: int, call: CallbackQuery) -> None:
+    students = student_storage.list_students(order_by='name')
+    pagination_result = paginate(
+        items=students, current_page=current_page, page_size=10
+    )
+    buttons = {}
 
-        case ReportBuilderChooseItemListCallbackData():
-            if data.i_t == 'T':
-                report_builder.set_topic_id_(topic_id=data.i_id)
-                process_bot_service_handler_results(
-                    bot_service.build_report_3_student_setting(
-                        data=ReportBuilderShowItemListCallbackData(
-                            i_t='S', page=FIRST_PAGE
-                        ),
-                    ),
-                    chat_id=user_id,
-                )
-            elif data.i_t == 'S':
-                report_builder.set_student_id_3(student_id=data.i_id)
-                report_builder.set_lesson_count_4(
-                    report_storage.lessons_count_by_student_id(student_id=data.i_id) + 1
-                )
-                process_bot_service_handler_results(
-                    bot_service.build_report_5_homework_status_setting(),
-                    chat_id=user_id,
-                )
+    for student in pagination_result.items:
+        buttons[student.name] = {
+            'callback_data': partial(show_one_student, student.id, current_page)
+        }
 
-        case ReportBuilder5SetHomeworkStatusCallbackData():
-            report_builder.set_homework_status_5(HomeworkStatus(data.homework_status))
-            process_bot_service_handler_results(
-                bot_service.build_report_6_is_proactive_setting(),
-                chat_id=user_id,
-            )
+    if not pagination_result.is_first_page:
+        buttons['Назад'] = {
+            'callback_data': partial(show_student_list, current_page - 1)
+        }
 
-        case ReportBuilder6SetIsProactiveCallbackData():
-            report_builder.set_is_proactive_6(bool(data.is_active))
-            process_bot_service_handler_results(
-                bot_service.build_report_7_payment_status_setting(),
-                chat_id=user_id,
-            )
+    if not pagination_result.is_last_page:
+        buttons['Вперёд'] = {
+            'callback_data': partial(show_student_list, current_page + 1)
+        }
 
-        case ReportBuilder7SetIsPaidCallbackData():
-            report_builder.set_is_paid_7(bool(data.payment_status))
-            process_bot_service_handler_results(
-                bot_service.build_report_8_ask_comment(),
-                chat_id=user_id,
-            )
-
-        case ReportBuilder8AddCommentQuestionCallbackData():
-            process_bot_service_handler_results(
-                *bot_service.build_report_8_get_comment(),
-                chat_id=user_id,
-            )
-
-        case ReportBuilderShowReportPreviewCallbackData():
-            process_bot_service_handler_results(
-                bot_service.build_report_preview(),
-                chat_id=user_id,
-            )
-
-        case SaveConfirmedReportCallbackData():
-            try:
-                complete_report = bot_service.save_report()
-                if data.parent_id:
-                    process_bot_service_handler_results(
-                        bot_service.send_report(complete_report),
-                        chat_id=data.parent_id,
-                    )
-            except ValidationError:
-                process_bot_service_handler_results(
-                    bot_service.get_error_message_temp_report_must_be_filled()
-                )
-
-            process_bot_service_handler_results(
-                *bot_service.welcome(user_id),
-                chat_id=user_id,
-            )
-
-        case SendSavedReportsCallbackData():
-            for message, report_id, parent_id in bot_service.send_saved_reports():
-                try:
-                    process_bot_service_handler_results(message, chat_id=parent_id)
-                    report_storage.set_is_sent(report_id)
-                except ApiTelegramException as e:
-                    if (
-                        e.error_code == 400
-                        and 'chat not found' in e.description.lower()
-                    ):
-                        process_bot_service_handler_results(
-                            'Пользователь (родитель) не найден. Проверьте id.',
-                            chat_id=parent_id,
-                        )
-
-        case ShowItemsListCallbackData():
-            process_bot_service_handler_results(
-                bot_service.show_items_list(data), chat_id=user_id
-            )
-
-        case ShowOneItemCallbackData():
-            process_bot_service_handler_results(
-                bot_service.show_one_item(data), chat_id=user_id
-            )
-
-        case DeleteOneItemCallbackData():
-            process_bot_service_handler_results(
-                bot_service.delete_one_item(data), chat_id=user_id
-            )
-
-        case DeleteConfirmedItemCallbackData():
-            process_bot_service_handler_results(
-                bot_service.delete_confirmed_one_item(data),
-                chat_id=user_id,
-            )
-
-        case other_callback_data:
-            print('other_callback_data', other_callback_data)
+    # buttons['Добавить студента'] = {'callback_data': partial(create_student, current_page)}
+    buttons['В меню'] = {'callback_data': partial(show_menu, current_page)}
+    telegram_bot.edit_message_text(
+        text='Выберите студента:',
+        chat_id=call.from_user.id,
+        message_id=call.message.id,
+        reply_markup=build_quick_markup(buttons),
+    )
 
 
 if __name__ == '__main__':
